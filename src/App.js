4ed9598e4bea17c4354e141d330865ab8312a0db
Gulasal Kuchkarova <gulasal.kuchkarova@stud.th-deg.de>
2022-12-24 07:32:54 -0500

    split components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Expenses />
     </div>
   );
 }
---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,18 @@
+import ExpenceDate from './ExpenceDate';
+import './ExpenceItem.css'
+
+function ExpenseItem(props){
+    // const month = props.date.toLocaleString('en-US',{month: 'long'});
+    // const day = props.date.toLocaleString('en-US', {day:'2-digit'});
+    // const year = props.date.getFullYear();
+    
+    return (
+        <div className='expense-item'>
+
+            <ExpenceDate date={props.date}/>
+            <div className='.expense-item__description'><h2>{props.title}</h2></div>
+            <div className='expense-item__price'>${props.amount}</div>
+        </div>
+    )
+}
+export default ExpenseItem;
