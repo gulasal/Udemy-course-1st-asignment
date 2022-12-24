3bb041dedf7a9c0deae7a8dab7d024611c6cc44e
Gulasal Kuchkarova <gulasalk424@gmail.com>
2022-12-24 13:04:01 -0500

    Add eventListener to the project

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -3,16 +3,16 @@ import Card from './Card';
 import './ExpenceItem.css'
 
 function ExpenseItem(props){
-    // const month = props.date.toLocaleString('en-US',{month: 'long'});
-    // const day = props.date.toLocaleString('en-US', {day:'2-digit'});
-    // const year = props.date.getFullYear();
+    const clickHandler = () => {
+        console.log("Clicked")
+    }
     
     return (
         <Card  className='expense-item'>
-
             <ExpenceDate date={props.date}/>
             <div className='.expense-item__description'><h2>{props.title}</h2></div>
             <div className='expense-item__price'>${props.amount}</div>
+            <button onClick={clickHandler}>Submit</button>
         </Card>
     )
 }
