fb2dd1462912ce07ff22a15a396f9e66815fb661
Gulasal Kuchkarova <gulasalk424@gmail.com>
2022-12-26 12:04:05 -0500

    Remove submit button

--- src/components/Expenses/ExpenceItem.js
+++ src/components/Expenses/ExpenceItem.js
@@ -1,24 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ExpenceDate from './ExpenceDate';
 import Card from '../UI/Card';
 import './ExpenceItem.css'
 
 function ExpenseItem(props){
-    const [title, setTitle] = useState(props.title);
-
     
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title); 
-    }
     
     return (
         <Card  className='expense-item'>
             <ExpenceDate date={props.date}/>
-            <div className='.expense-item__description'><h2>{title}</h2></div>
+            <div className='.expense-item__description'><h2>{props.title}</h2></div>
             <div className='expense-item__price'>${props.amount}</div>
-            <button onClick={clickHandler}>Submit</button>
         </Card>
     )
 }
